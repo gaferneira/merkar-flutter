b368f3258b93e45fc9136512f6fad086c60c283d
gabriel.neira <gneiragb@sofi.org>
2021-05-22 12:43:05 -0500

    Create sliver fab

--- lib/app/pages/home/home_page.dart
+++ lib/app/pages/home/home_page.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:merkar/app/pages/home/widgets/shopping_lists_display.dart';
-import 'package:merkar/app/pages/shopping/shopping_list/shopping_list_page.dart';
-import 'package:merkar/data/entities/shopping_list.dart';
-import 'package:merkar/app/core/extensions/extended_string.dart';
 import 'package:provider/provider.dart';
 
 import '../../../app/widgets/widgets.dart';
@@ -11,6 +8,7 @@ import '../../../injection_container.dart';
 import '../../core/resources/strings.dart';
 import '../shopping//new_shopping_list/new_shopping_list_page.dart';
 import 'home_view_model.dart';
+import 'widgets/sliver_fab.dart';
 
 class HomePage extends StatefulWidget {
   static const routeName = '/home';
@@ -45,82 +43,48 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    //var providerTheme = Provider.of<ProviderTheme>(context);
-    final _scaffKey = GlobalKey<ScaffoldState>();
+    final _scaffoldKey = GlobalKey<ScaffoldState>();
     return ChangeNotifierProvider<HomePageViewModel>.value(
       value: viewModel,
       child: Consumer<HomePageViewModel>(
         builder: (context, model, child) => Scaffold(
-          key: _scaffKey,
-          /*appBar: AppBar(
-            title: Container(
-              width: double.infinity,
-              height: 50.0,
-              decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                colors: [Colors.white70, Colors.white70],
-
-                // colors: [Colors.cyan[300], Colors.cyan[800]]
-              )),
-              child: ZoomIn(
-                duration: Duration(
-                  seconds: 1,
-                ),
-                child: Image.asset(
-                  'assets/images/market.png',
-                  width: 10.0,
-                ),
-              ),
+          key: _scaffoldKey,
+          body: new SliverContainer(
+            floatingActionButton: FloatingActionButton(
+              heroTag: "new_list",
+              onPressed: _goToCreateList,
+              tooltip: Strings.label_tootip_new_list,
+              child: Icon(Icons.add),
             ),
-            actions: <Widget>[
-              /* Switch(
-                  value: providerTheme.light,
-                  onChanged: (toggle) {
-                    providerTheme.updateTheme(toggle);
-                    setState(() {
-                      // providerTheme.light = toggle;
-                      print(
-                          "sent a value: ${providerTheme.light} to main.dart");
-                      changeTheme();
-                      print("Providertheme.ligh = ${providerTheme.light}");
-                    });
-                  }),*/
-              /*  Switch(
-                value: Provider.of<AppStateNotifier>(context).isDarkMode,
-                onChanged: (boolVal) {
-                  Provider.of<AppStateNotifier>(context).updateTheme(boolVal);
-                },
-              )*/
-            ],
-          ),
-           drawer: DrawerWelcome(),
-           */
-
-          body: CustomScrollView(
-            slivers:[
-              SliverAppBar(
+            expandedHeight: 256.0,
+            slivers: <Widget>[
+              new SliverAppBar(
+                iconTheme: IconThemeData(color: Colors.white),
+                expandedHeight: 256.0,
                 pinned: true,
-                floating: false,
-                expandedHeight: 160.0,
-                flexibleSpace: FlexibleSpaceBar(
-                  title: Text(Strings.label_name_app),
-                  background: Image.asset(
-                    'assets/images/logo.png',
-                    fit: BoxFit.cover,
-                  )
+                flexibleSpace: new FlexibleSpaceBar(
+                  title: Row(
+                    children: [
+                      Image.asset(
+                        'assets/images/app_logo.png',
+                        fit: BoxFit.fitHeight,
+                        height: 35,
+                        width: 35,
+                      ),
+                      Text(Strings.label_name_app),
+                    ],
+                  ),
+                  background: SizedBox(),
                 ),
               ),
-             const SliverToBoxAdapter(child: SizedBox(height: 30.0,)),
+              const SliverToBoxAdapter(
+                  child: SizedBox(
+                height: 30.0,
+              )),
               (viewModel.list == null)
                   ? SliverFillRemaining(child: Center(child: LoadingWidget()))
                   : shoppingListsDisplay(viewModel.list!),
             ],
-          ),
-          floatingActionButton: FloatingActionButton(
-            heroTag: "new_list",
-            onPressed: _goToCreateList,
-            tooltip: Strings.label_tootip_new_list,
-            child: Icon(Icons.add),
           ),
         ),
       ),
