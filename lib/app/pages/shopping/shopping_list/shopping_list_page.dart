e7ae2cd6eaa77e4032adf54b175773271ef92484
stip.suarez@gmail.com <stip90101678120>
2021-06-15 21:04:42 -0500

    share file ok

--- lib/app/pages/shopping/shopping_list/shopping_list_page.dart
+++ lib/app/pages/shopping/shopping_list/shopping_list_page.dart
@@ -1,4 +1,6 @@
 
+import 'dart:io';
+
 import 'package:animate_do/animate_do.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -6,6 +8,7 @@ import 'package:merkar/app/core/extensions/numberFormat.dart';
 import 'package:merkar/app/pages/products/select_products/select_products_page.dart';
 import 'package:merkar/app/widgets/loading_widget.dart';
 import 'package:provider/provider.dart';
+import 'package:share_plus/share_plus.dart';
 
 import '../../../../data/entities/list_product.dart';
 import '../../../../data/entities/shopping_list.dart';
@@ -113,7 +116,10 @@ class _ShoppingListPageState extends State<ShoppingListPage>
                           ),
                         ),
                       ),
-                      IconButton(icon: Icon(Icons.share), onPressed: () {}),
+                      IconButton(icon: Icon(Icons.share), onPressed: () {
+                        viewModel.getListFile();
+
+                      }),
                     ],
                   ),
                   body: SingleChildScrollView(
--- lib/app/pages/shopping/shopping_list/shopping_list_view_model.dart
+++ lib/app/pages/shopping/shopping_list/shopping_list_view_model.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-
+import 'dart:io';
+import 'package:path_provider/path_provider.dart';
+import 'package:share_plus/share_plus.dart';
 import '../../../../data/entities/list_product.dart';
 import '../../../../data/entities/shopping_list.dart';
 import '../../../../data/repositories/purchases_repository.dart';
@@ -21,6 +23,8 @@ class ShoppingListViewModel extends ChangeNotifier {
   List<ListProduct>? selectedList;
   String? error;
   bool? ennable;
+  File? _listFile;
+  final Future<Directory> localPath = getTemporaryDirectory();
 
   Future<void> loadData(ShoppingList shoppingList) async {
     this.shoppingList = shoppingList;
@@ -114,7 +118,32 @@ class ShoppingListViewModel extends ChangeNotifier {
     repository.removeProduct(productId, list);
     notifyListeners();
   }
+
   Future<void> removeList(ShoppingList shoppingList) async{
     repository.remove(shoppingList);
+  }
+
+  Future<File> getListFile() async {
+    final directory = localPath;
+    final file = await _localFile;
+    String content='Producto \b Cantidad\n';
+    for(int i=0;i<selectedList!.length;i++) {
+      content+='${selectedList![i].name} \b ${selectedList![i].quantity}\n';
+    }
+    file.writeAsString(content);
+    print('File ok ${file.path}');
+    Share.shareFiles(['${file.path}']);
+    return _listFile=file;
+
+  }
+
+  Future<String> get _localPath async {
+    final directory = await getTemporaryDirectory();
+    return directory.path;
+  }
+
+  Future<File> get _localFile async {
+    final path = await _localPath;
+    return File('$path/${shoppingList.name}.txt');
   }
 }
